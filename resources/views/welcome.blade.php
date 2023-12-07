50fe8d4b86831e06643d1c893997ea62525734ba
alirezakohandani <alireza.ko73@gmail.com>
2023-12-07 15:29:35 +0330

    Add livewire to the project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
+    @livewireStyles
 </head>
 <body>
 <h1 class="text-3xl font-bold underline font-iransans">
     Hello world!
+    @livewireScripts
+    <livewire:test />
 </h1>
 </body>
 </html>
