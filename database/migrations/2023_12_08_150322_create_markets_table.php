5fb13a4033953a63f2d02a5cc03ec243f0d6e1c9
Omid <omid1359@gmail.com>
2023-12-08 20:14:58 +0330

    refactor migration market

--- database/migrations/2023_12_08_150322_create_markets_table.php
+++ database/migrations/2023_12_08_150322_create_markets_table.php
@@ -15,32 +15,30 @@ return new class extends Migration
             $table->id();
             $table->string('name_en')->unique()->index();
             $table->string('name_fa')->unique()->index();
-            $table->string('description')->nullable();
-            $table->string('slug')->nullable()->unique();
+            $table->string('slug')->unique();
             $table->string('logo')->nullable();
-            $table->tinyInteger('status')->default(1)->comment('1:active 2:inactive');
+            $table->boolean('is_active')->default(1);
             $table->timestamps();
             $table->softDeletes();
         });
 
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->unique()->index();
+            $table->string('name')->unique()->index();
             $table->string('description')->nullable();
-            $table->string('slug')->nullable()->unique();
+            $table->string('slug')->unique();
             $table->string('icon')->nullable();
             $table->string('image')->nullable();
-            $table->tinyInteger('status')->default(1)->comment('1:active 2:inactive');
-            $table->tinyInteger('show_in_menu')->default(1)->comment('1:show 2:hidden');
-            $table->foreignId('parent_id')->nullable()->constrained('categories')->onDelete('cascade')->onUpdate('cascade');
+            $table->boolean('is_active')->default(1);
+            $table->foreignId('parent_id')->nullable()->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
             $table->softDeletes();
         });
 
         Schema::create('attributes', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->unique()->index();
-            $table->tinyInteger('status')->default(1)->comment('1:active 2:inactive');
+            $table->string('name')->unique()->index();
+            $table->boolean('is_active')->default(1);
             $table->string('slug')->nullable()->unique();
             $table->timestamps();
             $table->softDeletes();
@@ -48,29 +46,29 @@ return new class extends Migration
 
         Schema::create('attribute_category', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('attribute_id')->constrained('attributes')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade')->onUpdate('cascade');
-            $table->enum('is_filter', [1,2])->default(1)->comment('1: deactive, 2:active');
-            $table->enum('is_variation', [1,2])->default(1)->comment('1: is not variation, 2: is variation');
+            $table->foreignId('attribute_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->boolean('is_filter')->default(0);
+            $table->boolean('is_variation')->default(0);
             $table->timestamps();
             $table->softDeletes();
+            $table->primary(['category_id','attribute_id']);
         });
         
 
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->unique()->index();
+            $table->string('name')->unique()->index();
             $table->string('short_description');
             $table->text('long_description')->nullable();
-            $table->string('slug')->nullable()->unique();
+            $table->string('slug')->unique();
             $table->string('sku')->unique();
-            $table->string("variable")->nullable()->comment("for example: size, model, color & ...");
-            $table->foreignId('user_id')->nullable()->constrained('users')->cascadeOnUpdate()->nullOnDelete();
-            $table->foreignId('brand_id')->constrained('brands')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade')->onUpdate('cascade');
-            $table->string('image');
-            $table->tinyInteger('marketable')->default(1);
-            $table->tinyInteger('status')->default(1)->comment('1:active 2:inactive');
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnUpdate()->nullOnDelete();
+            $table->foreignId('brand_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('primary_image');
+            $table->boolean('marketable')->default(1);
+            $table->boolean('is_active')->default(1);
             $table->timestamp('published_at')->default(now());
             $table->timestamps();
             $table->softDeletes();
@@ -79,22 +77,22 @@ return new class extends Migration
 
         Schema::create('attribute_product', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('attribute_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->string('value')->nullable();
-            $table->foreignId('attribute_id')->constrained('attributes')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('product_id')->constrained('products')->onDelete('cascade')->onUpdate('cascade')->nullable();
-            $table->tinyInteger(' status')->default(1)->comment('1:active 2:inactive');
+            $table->boolean('is_active')->default(1);
             $table->timestamps();
             $table->softDeletes();
         });
 
-        Schema::create('product_variation', function (Blueprint $table) {
+        Schema::create('product_variations', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('attribute_id')->constrained('attributes')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('product_id')->constrained('products')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('attribute_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string("sku")->unique();
             $table->bigInteger('original_price');
             $table->bigInteger('sale_count')->default(0);
             $table->bigInteger('reservation_count')->default(0);
-            $table->string("sku")->unique();
             $table->bigInteger('buy_price');
             $table->boolean("has_discount")->default(0);
             $table->boolean("has_limited")->default(0);
@@ -105,7 +103,7 @@ return new class extends Migration
             $table->integer("tax_percent")->default(0)->comment("0% to 100%");
             $table->bigInteger('marketable_number')->default(0);
             $table->tinyInteger('marketable')->default(1);
-            $table->tinyInteger('status')->default(1)->comment('1:active 2:inactive');
+            $table->boolean('is_active')->default(1);
             $table->boolean('downloadable')->default(0)->comment('1: downloadable 0: no downloadable');
             $table->string("url")->nullable()->comment('download link for downloadable ware');
             $table->double("height")->default(0);
@@ -118,14 +116,6 @@ return new class extends Migration
             $table->softDeletes();
         });
 
-        Schema::create('attribute_product', function (Blueprint $table) {
-            $table->id();
-            $table->string('value')->nullable();
-            $table->foreignId('attribute_id')->constrained('attributes')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreignId('product_id')->constrained('products')->onDelete('cascade')->onUpdate('cascade')->nullable();
-            $table->timestamps();
-            $table->softDeletes();
-        });
 
     }
 
@@ -134,6 +124,13 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('markets');
+        Schema::dropIfExists('product_variations');
+        Schema::dropIfExists('attribute_product');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('attribute_category');
+        Schema::dropIfExists('attributes');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('brands');
+
     }
 };
