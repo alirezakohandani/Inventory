04f8a945d6eed8ead2d2186423ce85a7ca0edca0
Omid <omid1359@gmail.com>
2023-12-08 22:17:57 +0330

    Add migration user and opt

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,38 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string("username")->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string("avatar")->nullable();
+            $table->string("gender")->nullable();
+            $table->timestamp("birth_at")->nullable();
+
+            $table->boolean("is_admin")->default(0);
+            $table->boolean("is_active")->default(1);
+
+            $table->string('email')->nullable()->unique();
+            $table->timestamp('email_confirmed_at')->nullable();
+            
+            $table->string("mobile")->unique();
+            $table->timestamp("mobile_confirmed_at")->nullable();
+
+            $table->string("code_melli")->nullable();
+            $table->timestamp("code_melli_confirmed_at")->nullable();
+
+            $table->string('password')->nullable();
+            $table->boolean("password_force_change")->default(0);
+            $table->timestamp("password_expires_at")->nullable();
+            
             $table->rememberToken();
             $table->timestamps();
+        });
+
+
+        Schema::create('otps', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string("code");
+            $table->timestamp("expires_at")->nullable();
         });
     }
 
@@ -27,6 +53,8 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('otps');
         Schema::dropIfExists('users');
+
     }
 };
