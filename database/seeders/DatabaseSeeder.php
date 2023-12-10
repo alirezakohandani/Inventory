57d4bcb64be11b2ab048d9703543e4e27bf054da
alirezakohandani <alireza.ko73@gmail.com>
2023-12-10 09:23:46 +0330

    Refactor division seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\DivitionDatabaseSeeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,14 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
-         $this->call(DivitionDatabaseSeeder::class);
-
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(DivisionDatabaseSeeder::class);
     }
 }
